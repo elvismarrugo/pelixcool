5e77835be01456b4446656a8652e1dec3f4d302c
elvismarrugo <elvismarrugo@gmail.com>
2021-02-14 13:38:26 -0500

    add server

--- pages/_app.js
+++ pages/_app.js
@@ -4,3 +4,7 @@ import 'semantic-ui-css/semantic.min.css';
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
+
+
+// mongodb+srv://admin:Cloudmngdb$0852@cluster0.q5f5a.mongodb.net/pelixcooldb?retryWrites=true&w=majority
+// mongodb+srv://admin:Cloudmngdb$0852@pelixcluster.q5f5a.mongodb.net/pelixcooldb?retryWrites=true&w=majority
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,28 @@
 import BasicLayout from '../layouts/BasicLayout'
 
-export default function Home({ movie }) {
+export default function Home({ movies }) {
+  
   return (
-    <BasicLayout>      
+    <BasicLayout>
       <div>
-        {/* <h6>{JSON.stringify(movie)}</h6> */}
-        {/* <img src={movie.Poster} alt='' /> */}
-
-        <h6>{`${movie.Title} - ${movie.Year}`}</h6>
-        
+        {/* <h6>{JSON.stringify(movies)}</h6> */}
+        {/* <img src={movies.Poster} alt='' /> */}
+        {/* <h6>{`${movies.Title} - ${movies.Year}`}</h6>     */}
+        {
+          movies.map((movie) => {
+            return <div>{`${movie.Title} - ${movie.Year}`}</div>;
+          })
+        }
       </div>
     </BasicLayout>
   );
 }
 
-Home.getInitialProps = async (query) => {  
-  const res = await fetch(
-    'http://www.omdbapi.com/?i=tt3896198&apikey=8f82567b'
+Home.getInitialProps = async ({query}) => {
+  const pagina = query.pagina ? Number(query.pagina) : 1
+  const res = await fetch(    
+    `http://www.omdbapi.com/?apikey=8f82567b&s=Batman&page=${pagina}`    
   );
-  const json = await res.json();
-  return { movie: json};
+  const json = await res.json();  
+  return { movies: json.Search };  
 };
