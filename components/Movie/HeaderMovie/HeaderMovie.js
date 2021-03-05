e9c76cfb85121ee2618c3f86aa1e09c16d606df0
Elvix Marrugo <elvismarrugo@gmail>
2021-03-04 22:11:05 -0500

    headermovie

--- components/Movie/HeaderMovie/HeaderMovie.js
+++ components/Movie/HeaderMovie/HeaderMovie.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { Grid, Image } from "semantic-ui-react";
-// import { size } from "lodash";
-// import classNames from "classnames";
-// import useAuth from "../../../hooks/useAuth";
+import React, { useState, useEffect } from "react";
+import { Grid, Image, Icon } from "semantic-ui-react";
+import { size } from "lodash";
+import classNames from "classnames";
+import useAuth from "../../../hooks/useAuth";
 // import useCart from "../../../hooks/useCart";
 // import {
 //   isFavoriteApi,
@@ -12,17 +12,67 @@ import { Grid, Image } from "semantic-ui-react";
 
 export default function HeaderMovie(props) {
   const { movie } = props;
-  console.log(movie)
-  // const { poster, title } = movie;
-
-  // const  movie   = props;
-  // console.log(movie)
+  const { Poster, Title } = movie.movie;
 
   return (
     <Grid className="header-movie">
       <Grid.Column mobile={16} tablet={6} computer={5}>
         <Image src={Poster} alt={Title} fluid />
       </Grid.Column>
-    </Grid>
+      <Grid.Column mobile={16} tablet={10} computer={11}>
+        <Info movie={movie} />
+      </Grid.Column>
+    </Grid>  
+  );
+}
+
+function Info(props) {
+  const { movie } = props;
+  const { Title, Plot } = movie.movie;;
+  const [isFavorite, setIsFavorite] = useState(false);
+  const [realoadFavorite, setRealoadFavorite] = useState(false);
+  const { auth, logout } = useAuth();
+
+  useEffect(() => {
+    (async () => {
+      const response = await isFavoriteApi(auth.idUser, movie.id, logout);
+      if (size(response) > 0) setIsFavorite(true);
+      else setIsFavorite(false);
+    })();
+    setRealoadFavorite(false);
+  }, [movie, realoadFavorite]);
+
+  const addFavorite = async () => {
+    if (auth) {
+      await addFavoriteApi(auth.idUser, movie.id, logout);
+      setRealoadFavorite(true);
+    }
+  };
+
+  const deleteFavorite = async () => {
+    if (auth) {
+      await deleteFavoriteApi(auth.idUser, movie.id, logout);
+      setRealoadFavorite(true);
+    }
+  };
+
+  return (
+    <>
+      <div className="header-movie__title">
+        {Title}
+        <Icon
+          name={isFavorite ? "heart" : "heart outline"}
+          className={classNames({
+            like: isFavorite,
+          })}
+          link
+          onClick={isFavorite ? deleteFavorite : addFavorite}
+        />
+      </div>
+      <div
+        className="header-movie__Plot"
+        dangerouslySetInnerHTML={{ __html: Plot }}
+      />
+    </>
   );
 }
