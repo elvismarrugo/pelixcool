d1ee0b681799c73863e8393c9d2034b0943c18e0
MrElvix <elvismarrugo@gmail.com>
2021-03-12 10:35:56 -0500

    https

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,4 @@
 // export const BASE_PATH = 'http://localhost:1337';
-export const BASE_PATH = 'http://pelixcool.herokuapp.com';
-export const BASE_URLAPI = 'http://www.omdbapi.com/?apikey=8f82567b';
+export const BASE_PATH = 'https://pelixcool.herokuapp.com';
+export const BASE_URLAPI = 'https://www.omdbapi.com/?apikey=8f82567b';
 export const TOKEN = "token";
